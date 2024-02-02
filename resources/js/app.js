b6d7e81e3fa77aca01d5872e7a150aaf18e9160c
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-02 16:46:16 +0700

    fix: missing vuex import

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,13 @@ import '../sass/app.scss'
 
 import { createApp } from 'vue';
 import Router from '@/router'
+import store from "@/store";
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp({});
 
 app.use(Router)
+app.use(store)
 app.use(VueSweetalert2)
 app.mount('#app');
