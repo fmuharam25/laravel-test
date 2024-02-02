078d4a4adcf142b0cbe287b11d5fc96e7a1af039
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-02 12:56:51 +0700

    chore: initial setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+import '../sass/app.scss'
+
+import { createApp } from 'vue';
+import Router from '@/router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const app = createApp({});
+
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
+app.use(Router)
+app.use(VueSweetalert2)
+app.mount('#app');
