7a491543b5c9a8e1e684d4369fd37d4b567a49fe
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-02 22:40:24 +0700

    chore: craete router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,1 +1,83 @@
-s
+import { createWebHistory, createRouter } from 'vue-router'
+import store from '@/store'
+
+
+const AppLayout = () => import('@/components/App.vue')
+const Login = () => import('@/components/pages/Login.vue')
+const Register = () => import('@/components/pages/Register.vue')
+const Dashboard = () => import('@/components/pages/Dashboard.vue')
+const UserList = () => import('@/components/pages/UserList.vue')
+
+
+const routes = [
+    {
+        name: "login",
+        path: "/login",
+        component: Login,
+        meta: {
+            middleware: "guest",
+            title: `Login`
+        }
+    },
+    {
+        name: "register",
+        path: "/register",
+        component: Register,
+        meta: {
+            middleware: "guest",
+            title: `Register`
+        }
+    },
+    {
+        path: "/",
+        component: AppLayout,
+        meta: {
+            middleware: "auth"
+        },
+        children: [
+            {
+                name: "dashboard",
+                path: '/',
+                component: Dashboard,
+                meta: {
+                    title: `Dashboard`
+                }
+            },
+            {
+                name: "user-list",
+                path: '/user-list',
+                component: UserList,
+                meta: {
+                    title: `User Data`
+                }
+            }
+        ]
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title
+    
+    if (to.meta.middleware == "guest") {
+        if (store.state.auth.authenticated) {
+            next({ name: "dashboard" })
+        }
+        next()
+    } else {
+        if (store.state.auth.authenticated) {
+            if(store.state.auth.user.role == 'guest' && to.name == 'user-list'){
+                next({ name: "dashboard" })
+            }
+            next()
+        } else {
+            next({ name: "login" })
+        }
+    }
+})
+
+export default router
