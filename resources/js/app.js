85f7b544b582359339696cdc90361c486fdbe462
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-02 16:02:15 +0700

    chore: remove unused component

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,8 +8,6 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
 app.use(Router)
 app.use(VueSweetalert2)
 app.mount('#app');
