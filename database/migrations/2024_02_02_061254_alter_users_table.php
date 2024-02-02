968ed1884d8d75bab8bcbc6088c2a9ac9aefc3ac
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-02 13:15:59 +0700

    chore: create user migration and seeder

---
+++ database/migrations/2024_02_02_061254_alter_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username',20);
+            $table->string('phone',30)->nullable()->default(null);
+            $table->enum('role', ['guest', 'admin'])->default('guest');
+            $table->enum('is_subscribe', ['subscribed', 'unsubscribe'])->default('subscribed');
+
+
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('phone');
+            $table->dropColumn('role');
+            $table->dropColumn('is_subscribe');
+        });
+    
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'username' => 'Administrator',
+            'name' => 'Administrator',
+            'email' => 'admin@growmodo.com',
+            'phone' => '123456789',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'subscription' => 'subscribed'
+        ]);
+
+    }
+}
